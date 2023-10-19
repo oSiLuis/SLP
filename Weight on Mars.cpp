7098a146947e6b54a8011ac6dbb9de1877c867be
oSiLuis <143099490+oSiLuis@users.noreply.github.com>
2023-10-18 20:19:14 -0500

    Add files via upload

---
+++ Weight on Mars.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	double mars = 3.73 / 9.81;
+	double weight;
+
+	std::cout << "Please enter your weight in lbs:";
+	std::cin >> weight;
+	std::cout << (weight * mars) << "\n";
+
+	return 0;
+}
